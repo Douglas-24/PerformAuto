ae11aabc6cd50fd6d7fa7f39e7feff08717b58bb
Alvaro Douglas <douglastiburcio24@gmail.com>
2025-10-21 12:08:57 +0200

    Arreglo en estructura de carpetas

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { HomeComponent } from './app/components/home-component/home-component';
-bootstrapApplication(HomeComponent, appConfig)
+import { App } from './app/app';
+bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
