6d19a672aadbcfbbfe60c236a8dc1f0cdd98de7d
Alvaro Douglas <douglastiburcio24@gmail.com>
2025-11-03 01:13:58 +0100

    Navegador lateral componetizado

---
+++ front/src/app/features/profile/components/aside-nav/aside-nav.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aside-nav',
+  imports: [],
+  templateUrl: './aside-nav.html',
+  styleUrl: './aside-nav.css'
+})
+export class AsideNav {
+
+}
--- front/src/app/features/profile/pages/profile-pages/profile-pages.ts
+++ front/src/app/features/profile/pages/profile-pages/profile-pages.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../../../../shared/header/header';
 import { Footer } from '../../../../shared/footer/footer';
+import { AsideNav } from '../../components/aside-nav/aside-nav';
 @Component({
   selector: 'app-profile-pages',
-  imports: [Header, Footer],
+  imports: [Header, Footer, AsideNav],
   templateUrl: './profile-pages.html',
   styleUrl: './profile-pages.css'
 })
