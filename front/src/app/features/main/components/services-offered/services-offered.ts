2de5dddd6ec886f80ac38f7cb5cae2f662cb9586
Alvaro Douglas <douglastiburcio24@gmail.com>
2025-12-03 17:38:12 +0100

    Añadido servicios en la pagina principal

---
+++ front/src/app/features/main/components/services-offered/services-offered.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-services-offered',
+  imports: [RouterLink],
+  templateUrl: './services-offered.html',
+  styleUrl: './services-offered.css'
+})
+export class ServicesOffered {
+
+}
