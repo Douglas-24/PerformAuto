81cb95c22030530284fd0a875ed36a03c19faf42
Alvaro Douglas <douglastiburcio24@gmail.com>
2025-12-09 22:38:38 +0100

    Crud piezas

--- front/src/app/features/parts/components/parts-table/parts-table.ts
+++ front/src/app/features/parts/components/parts-table/parts-table.ts
@@ -5,7 +5,8 @@ import { DinamicTable } from '../../../../shared/dinamic-table/dinamic-table';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ConfigFieldsForm } from '../../../../core/interfaces/configFiledsForm';
 import { PartsService } from '../../../../core/service/parts.service';
-import { Parts } from '../../../../core/interfaces/parts.interfaces';
+import { Parts, PartUsed } from '../../../../core/interfaces/parts.interfaces';
+import { DinamicModal } from '../../../../shared/dinamic-modal/dinamic-modal';
 @Component({
   selector: 'app-parts-table',
   imports: [DinamicTable],
@@ -17,7 +18,7 @@ export class PartsTable implements OnInit {
   private toast = inject(ToastServices)
   private modal = inject(Dialog)
 
-  allParts:Parts[] = []
+  allParts: PartUsed[] = []
   partsForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
     references: new FormControl('', [Validators.required]),
@@ -26,28 +27,105 @@ export class PartsTable implements OnInit {
   })
 
   configFields: ConfigFieldsForm[] = [
-    {key: 'name', label: 'Nombre', type: 'text'},
-    {key: 'reference', label: 'Nº referencia', type: 'text'},
-    {key: 'price', label: 'Precio', type: 'text'},
-    {key: 'stock', label: 'Stock', type: 'text'},
+    { key: 'name', label: 'Nombre', type: 'text' },
+    { key: 'references', label: 'Nº referencia', type: 'text' },
+    { key: 'price', label: 'Precio', type: 'text' },
+    { key: 'stock', label: 'Stock', type: 'text' },
   ]
 
   ngOnInit(): void {
     this.getAllParts()
   }
 
-  getAllParts(){
-    this.partsService.getAllParts().subscribe({
-      next:(resp)=>{
-        this.allParts = resp.data
-        console.log(resp);
-        
-      },
-      error: (error) =>{
-        console.log(error);
-        
+  async openModal(part?: PartUsed, deleteAction?: boolean) {
+    const title = !part ? 'Crear nueva pieza' : part.name
+    const typeModal = part ? 'Actualizar' : 'Crear'
+    const modalRef = this.modal.open(DinamicModal, {
+      data: {
+        title: !deleteAction ? title : 'Eliminar pieza',
+        formGroup: !deleteAction ? this.partsForm : null,
+        configFields: !deleteAction ? this.configFields : null,
+        message: deleteAction ? 'Estas seguro de que quieres eliminar la pieza' : '',
+        typeModal: deleteAction ? 'Confirmar' : typeModal,
+        updateDataForm: part
       }
     })
+    const confimed = await modalRef.closed.toPromise()
+    if (confimed) {
+      if(deleteAction && part){
+        if(part.id) this.deletePart(part.id)
+      }else{
+        part ? this.updatePart(part) : this.createPart()
+      }
+    } else {
+      this.partsForm.reset()
+    }
+  }
+
+  getAllParts() {
+    this.partsService.getAllParts().subscribe({
+      next: (resp) => {
+        this.allParts = resp.data
+        console.log(resp);
+
+      },
+      error: (error) => {
+        console.log(error);
+
+      }
+    })
+  }
+
+  createPart() {
+    const formValue = this.partsForm.value
+    const partData: Parts = {
+      name: formValue.name!,
+      reference: formValue.references!,
+      price: parseInt(formValue.price!),
+      stock: parseInt(formValue.stock!)
+    }
+    this.partsService.postPart(partData).subscribe({
+      next: (resp) => {
+        this.getAllParts()
+        this.toast.show('Pieza creada', resp.message, 'success')
+      },
+      error: (error) => {
+        console.log(error);
+
+      }
+    })
+  }
+  updatePart(part: PartUsed) {
+    const formValue = this.partsForm.value
+    const partData: Parts = {
+      name: formValue.name!,
+      reference: formValue.references!,
+      price: parseInt(formValue.price!),
+      stock: parseInt(formValue.stock!)
+    }
+    this.partsService.updatePart(part.id, partData).subscribe({
+      next: (resp) => {
+        this.getAllParts()
+        this.toast.show('Pieza actualizada', resp.message, 'success')
+      },
+      error: (error) => {
+        console.log(error);
+
+      }
+    })
+  }
+
+  deletePart(id:number){
+    this.partsService.deletePart(id).subscribe({
+      next: (resp) => {
+        this.toast.show('Accion exitosa', resp.message, 'success')
+        this.getAllParts()
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
+
   }
 
 }
